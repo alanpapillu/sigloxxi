3fc7b1824df717c6dec7d998d36a43b94cde529d
Alan <alan.papillu@hotmail.com>
2023-11-17 23:06:13 -0300

    primer commit

---
+++ src/main/java/com/sigloxxi/springtodolist/SpringtodolistApplication.java
@@ -0,0 +1,13 @@
+package com.sigloxxi.springtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sigloxxi/springtodolist/SpringtodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigloxxi.springtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
